946062cf51f26a4cf6cbba358e4b9157c9062448
Andrew Wilhelm <abwilhelm9013@eagle.fgcu.edu>
2022-11-08 15:43:47 -0500

    Better format for Pull data from FGCU Women's Soccer stats table

--- Requests-with-Soup-and-selenium.py
+++ Requests-with-Soup-and-selenium.py
@@ -8,8 +8,19 @@ Will display (print) which season they did the best
 """
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys     # literally keys on the keyboard for input
+from selenium.webdriver.common.keys import Keys  # literally keys on the keyboard for input
 from selenium.webdriver.common.by import By
+
+
+def makerows(row):
+    words = ""
+
+    for i, letter in enumerate(row):
+        if not letter.isdigit():
+            words += letter
+        else:
+            return [words.strip(), *row[i:].split(" ")]
+
 
 if __name__ == '__main__':
     driver = webdriver.Chrome()
@@ -24,8 +35,10 @@ if __name__ == '__main__':
     table_data = driver.find_elements(By.XPATH, "//table[1]/tbody/tr")
     for row in table_data:
         if row.text:
-            cur_row = row.text.split(" ")
-            if len(cur_row) != 1:
+            cur_row = makerows(row.text)
+            if cur_row is None or len(cur_row) == 1:
+                continue
+            else:
                 table_rows.append(cur_row)
 
     for i, row in enumerate(table_rows):
